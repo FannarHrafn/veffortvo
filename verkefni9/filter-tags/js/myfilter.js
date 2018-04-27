82dbb7ab14bcfb72581c1730e2d3049ae42320e3
FannarHrafn <fannarhrafn@gmail.com>
2018-04-27 10:07:22 +0000

    Update myfilter.js

--- verkefni9/filter-tags/js/myfilter.js
+++ verkefni9/filter-tags/js/myfilter.js
@@ -1,7 +1,7 @@
 "use strict";
-var data = [["p1.jpg","animators","illustrators"],["p2.jpg","photographers, filmmakers"],
-			["p3.jpg","photographers, filmmakers"],["p4.jpg","designers"],
-			["p5.jpg","photographers, filmmakers"],["p6.jpg","designers","illustrators"],
+var data = [["p1.jpg","animators","illustrators"],["p2.jpg","photographers","filmmakers"],
+			["p3.jpg","photographers", "filmmakers"],["p4.jpg","designers"],
+			["p5.jpg","photographers","filmmakers"],["p6.jpg","designers","illustrators"],
 			["p7.jpg","photographers"],["p8.jpg","designers"],["p9.jpg","animators","illustrators"]];
 var tags = [];
 for (var x = 0; x < data.length; x++){
@@ -13,7 +13,18 @@ for (var x = 0; x < data.length; x++){
 }
 for (var i = 0; i < tags.length; i++){
 	var node = document.createElement("button");              
-	var textnode = document.createTextNode(tags[i]);        
-	node.appendChild(textnode); 
+	var textnode = document.createTextNode(tags[i]);
+	node.appendChild(textnode);
 	document.getElementById("buttons").appendChild(node);
+	tags[i] = new Vue({
+		el: '#'+tags[i],
+		data: {
+			seen:true
+		}
+	});
+	window.console.log(tags[i]);
+}
+document.getElementById("buttons").addEventListener('click',magician,false);
+function magician(){
+	event.
 }
